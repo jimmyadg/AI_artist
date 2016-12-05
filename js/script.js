217735a57afa3bc6c1c5f4693da46a457733897c
jimmyadg <jimmyadg@gmail.com>
2016-12-05 10:08:42 -0500

    demo

--- js/script.js
+++ js/script.js
@@ -26,14 +26,6 @@ function init(){
   container.appendChild( renderer.domElement );
   loadImg();
 
-  var spritey = textRender("hello, ",{
-    fontsize:24,
-    borderColor:{r:255,g:0,b:0,a:1.0},
-    backgroundColor:{r:255,g:100,b:100,a:1.0}
-  });
-  spritey.position.set(-85,105,55);
-  scene.add(spritey);
-
   camera.position.z = 20;
   camera.lookAt(new THREE.Vector3(0,0,0));
   scene.add(camera);
@@ -51,57 +43,6 @@ function loadImg(){
     scene.add(plane);
   });
 }
-
-function textRender(message, parameters){
-  if(parameters === undefined) parameters ={};
-  var fontface = parameters.hasOwnProperty("fontface")?parameters["fontface"]:"Roboto";
-  var fontsize = parameters.hasOwnProperty("fontsize")?parameters["fontsize"]:18;
-  var borderThickness = parameters.hasOwnProperty("borderThickness")?parameters["borderThickness"]:4;
-  var borderColor = parameters.hasOwnProperty("borderColor")?parameters["borderColor"]:{r:0,g:0,b:0,a:1.0};
-  var backgroundColor = parameters.hasOwnProperty("backgroundColor")?parameters["backgroundColor"]:{r:255,g:255,b:255,a:1.0};
-  //var spriteAlignment = THREE.SpriteAlignment.topLeft;
-  var canvas = document.createElement('canvas');
-  var context = canvas.getContext('2d');
-  context.font = "Bold" + fontsize + "px "+ fontface;
-  var metrics = context.measureText(message);
-  var textWidth = metrics.width;
-  //background color
-  context.fillStyle = "rgba(" + backgroundColor.r +"."+backgroundColor.g+","+backgroundColor.b+","+backgroundColor.a+")";
-  //border color
-  context.strokeStyle = "rgba(" + backgroundColor.r +"."+backgroundColor.g+","+backgroundColor.b+","+backgroundColor.a+")";
-  context.lineWidth = borderThickness;
-  roundRect(context,borderThickness/2,borderThickness/2,textWidth+borderThickness,fontsize*1.4+borderThickness,6);
-
-  //text color
-  context.fillStyle="rgba(0,0,0,1.0)";
-  context.fillText(message,borderThickness,fontsize+borderThickness);
-  var texture = new THREE.Texture(canvas);
-  texture.needsUpdate = true;
-  var spriteMaterial = new THREE.spriteMaterial({
-    map:texture,
-    useScreenCoordinates: false
-    //alignment: spriteAlignment
-  });
-  var sprite = new THREE.Sprite(spriteMaterial);
-  sprite.scale.set(100,50,1.0);
-  return sprite;
-}
-
-//drawing rectangles
-function roundRect(ctx,x,y,w,h,r){
-  ctx.beginPath();
-  ctx.moveTo(x+r,y);
-  ctx.lineTo(x+w-r,y);
-  ctx.quadraticCurveTo(x+w,y,x+w,y+r);
-  ctx.lineTo(x+w,y+h-r);
-  ctx.quadraticCurveTo(x+w,y+h,x,y+h-r);
-  ctx.lineTo(x,y+r);
-  ctx.quadraticCurveTo(x,y,x+r,y);
-  ctx.closePath();
-  ctx.fill();
-  ctx.stroke();
-}
-
 
 function animatedRender(){
   requestAnimationFrame(animatedRender);
